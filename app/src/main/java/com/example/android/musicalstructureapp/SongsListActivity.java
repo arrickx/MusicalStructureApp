6525155f1bc344fc04f18db7ae2ba4acce68da76
arrickx <24638167+arrickx@users.noreply.github.com>
2018-05-27 14:57:20 -0400

    finish building custome classes and ArrayAdapter

--- app/src/main/java/com/example/android/musicalstructureapp/SongsListActivity.java
+++ app/src/main/java/com/example/android/musicalstructureapp/SongsListActivity.java
@@ -4,7 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class SongsListActivity extends AppCompatActivity {
 
@@ -13,20 +19,24 @@ public class SongsListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Find the View that shows the song
-        TextView numbers = (TextView) findViewById(R.id.song_name);
+        // Create an Array of songs
+        ArrayList<Song> songs = new ArrayList<>();
 
-        // Set a click listener on that View
-        numbers.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when the song is clicked on.
-            @Override
-            public void onClick(View view) {
-                // Create a new intent to open the {@link NumbersActivity}
-                Intent songsIntent = new Intent(SongsListActivity.this, NowPlayingActivity.class);
+        songs.add(new Song("song 1", "artist 1"));
+        songs.add(new Song("song 2", "artist 2"));
+        songs.add(new Song("song 3", "artist 3"));
 
-                // Start the new activity
-                startActivity(songsIntent);
-            }
-        });
+        // Create an {@link SongAdapter}, whose data source is a list of {@link Song}s. The
+        // adapter knows how to create list items for each item in the list.
+        SongAdapter adapter = new SongAdapter(this, songs);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // word_list.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
+        // {@link ListView} will display list items for each {@link Word} in the list.
+        listView.setAdapter(adapter);
     }
 }
